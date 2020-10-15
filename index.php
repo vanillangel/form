2a4287243b984ba03587e49e04bd6e52b19c5ee7
vanillangel <8vanillaangel8@gmail.com>
2020-10-15 17:23:19 +0400

    12

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<style type="text/css">
+		body{background-color: lightgray;}
+		form{border:1px solid black; width:300px; border-radius: 5px;
+			background-color: white;}
+			form h4 a{text-decoration: none;}
+form h4 a:link{ color:black; }
+form h4 a:visited{ color:black; }
+form h4 a:hover{ color:darkred; }
+
+	</style>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form>
+
+	<h4 align="center"><a href="views/sign_in.php">Sign In</a></h4>
+	<h4 align="center"><a href="views/sign_up.php">Sign Up</a></h4>
+</form>
+</body>
+</html>
---
+++ views/sign_in.php
@@ -0,0 +1,27 @@
+<? php
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<style type="text/css">
+		body{background-color: lightgray;}
+		form{border:1px solid black; width:300px; border-radius: 5px;
+			background-color: white;}
+		form input{margin:10px;}
+		form button {background-color: lightgray; display: block;
+    margin: 0 auto 15px auto;
+    text-align: center;border-radius: 5px;}
+	</style>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form method="POST" action="/controllers/sign_in.php">
+		<h4 align="center">Sign In</h4>
+		<input type="text" placeholder="username" name="si_username"><br>
+<input type="password" placeholder="password" name="si_password"><br>
+<button script="index.php">Enter</button>
+	</form>
+</body>
+</html>
